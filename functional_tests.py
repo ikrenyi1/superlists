f6bb7b6bd29cddb4e4b84b499a258a06c7da88ec
Pavel Ikrenyi <ikrenyi11@gmail.com>
2018-03-24 17:21:40 +0100

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -18,7 +18,7 @@ class NewVisitorTest(unittest.TestCase):
         # page title and header
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1')
-        self.assertIn('To-Do', header_text)
+        self.assertIn('To-Do', header_text.text)
 
         # invite to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -37,7 +37,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            msg="New to-do item did not appear in table"
         )
 
         self.fail('Finish the test!')
