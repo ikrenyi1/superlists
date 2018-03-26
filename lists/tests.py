f2193deaa07a1217dc9dcbd1ae72882b14a6c522
Pavel Ikrenyi <ikrenyi11@gmail.com>
2018-03-26 20:24:01 +0200

    new url, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -11,6 +11,10 @@ from lists.views import home_page
 
 class ListViewTest(TestCase):
 
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
     def test_dispalys_all_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
@@ -40,7 +44,6 @@ class ItemModelTest(TestCase):
 
 class HomePageTest(TestCase):
 
-
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
@@ -60,12 +63,3 @@ class HomePageTest(TestCase):
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
-
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        response = self.client.get('/')
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
