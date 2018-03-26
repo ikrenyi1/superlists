56ba1d6fe916385e6dcfffb32f6f29d8adb3e71c
Pavel Ikrenyi <ikrenyi11@gmail.com>
2018-03-26 19:49:36 +0200

    Added only-list-in-the-world url

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -26,9 +26,6 @@ class NewVisitorTest(LiveServerTestCase):
         )
 
     def wait_for_row_in_list_table(self, row_text):
-        #table = self.browser.find_element_by_id('id_list_table')
-        #rows = table.find_elements_by_tag_name('tr')
-        #self.assertIn(row_text, [row.text for row in rows])
 
         start_time = time.time()
         while True:
@@ -75,10 +72,42 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table(
             '2: Use peacock feathers to make a fly')
 
-    #def test_multiple_users_can_start_lists_at_different_urls(self):
-    #    self.browser.get(self.live_server_url)
-    #    inputbox = self.browser.find_element_by_id('id_new_item')
-    #    inputbox.send_keys('Buy peacock feathers')
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # user notices that their list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        # new user comes along to the site
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # francis visits the home page
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # francis starts a new list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # francis has hiw own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # no trace of ediths list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
--- lists/views.py
+++ lists/views.py
@@ -5,20 +5,13 @@ from django.http import  HttpResponse
 # Create your views here.
 
 def home_page(request):
-
-    #if request.method == 'POST':
-    #    new_item_text = request.POST['item_text']
-    #    Item.objects.create(text=new_item_text)
-    #else:
-    #    new_item_text = ''
-
-    #return render(request, 'home.html', {
-    #    'new_item_text': new_item_text,
-    #})
-
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
 
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
+
+def view_list(request):
     items = Item.objects.all()
     return render(request, 'home.html', {'items': items})
